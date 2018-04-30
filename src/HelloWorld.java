b2a475b6d893f904a014871feeada40b268f5dee
John Wallace <jnwywe@gmail.com>
2018-04-30 10:57:37 -0500

    Finished Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    System.out.print("I'm ");
+    System.out.print("John");
+}
+
+
+//practice single line comment
+    /*
+
+    *practice block comment
+     * */
+
+
+}
+
